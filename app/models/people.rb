435ff68fe91c76f27d8b79fb70584b2d1e3ee4e5
Patrick Ragi Helmi <Patricholamaro@yahoo.com>
2013-07-31 17:34:27 +0200

    Deleted People model
    Delete People model and migrations that was causing the conflict

--- app/models/people.rb
+++
@@ -1,7 +0,0 @@
-class People < ActiveRecord::Base
-  # attr_accessible :title, :body
-  belongs_to :group
-  has_and_belongs_to_many :group
-  attr_accessible :nid, :name, :group_id
-  scope :search ,lambda { |p| where (["name LIKE ?","%#{p}%"])}
-end
