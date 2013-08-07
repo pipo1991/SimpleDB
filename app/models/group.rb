9aafc606cfae71de648b27173eb570ce1852ca44
Patrick Ragi Helmi <Patricholamaro@yahoo.com>
2013-08-07 12:33:04 +0200

    updating models validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,15 +1,7 @@
 class Group < ActiveRecord::Base
   # attr_accessible :title, :body
   validates_presence_of :name
-  validate :group_restrict ,:on => :create
-  validate :group_restrict ,:on => :update
   has_and_belongs_to_many :human
   attr_accessible :id,:name ,:human ,:human_ids
-  
-  def group_restrict
-    if @group.human.count > 3
-      errors.add('',"Group can't have more than 4 members.")
-    end
-  end
   
 end
--- app/models/human.rb
+++ app/models/human.rb
@@ -1,7 +1,17 @@
 class Human < ActiveRecord::Base
   # attr_accessible :title, :body
+  validates_presence_of :name 
+  validates_presence_of :nid
+  validate :group_restrict ,:on => :create
+  validate :group_restrict ,:on => :update
   belongs_to :group
   has_and_belongs_to_many :group
   attr_accessible :id,:nid, :name, :group_id ,:group , :group_ids
   scope :search ,lambda { |p| where (["name LIKE ?","%#{p}%"])}
+  
+  def group_restrict
+    if @human.group.count > 3
+      errors.add('',"Group can't have more than 4 members.")
+    end
+  end
 end
