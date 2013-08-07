7e16b83da30ce6af9ce05a0f6a5fc91d87064512
Patrick Ragi Helmi <Patricholamaro@yahoo.com>
2013-08-07 16:23:23 +0200

    Add the check number of team members

--- app/controllers/human_controller.rb
+++ app/controllers/human_controller.rb
@@ -19,12 +19,23 @@ class HumanController < ApplicationController
   def create
     @people = Human.create(:name => params[:human][:name],:nid =>params[:human][:nid],:group_id => params[:human][:group_id])
     @people.group_ids = params[:human][:group_ids]
-    begin
-        @people.save
-    rescue
-       flash[:notice] = "Successfully created ..."
+    flag=true
+    for i in @human.group
+      if i.human.count>3
+        flag=false
+      end
+    end
+    if flag
+        begin
+            @people.save
+        rescue
+           flash[:notice] = "Successfully created ..."
+           redirect_to(:action => 'list')
+        ensure 
+        end
+     else
+       flash[:notice] = "Error one of the group already filled ..."
        redirect_to(:action => 'list')
-    ensure 
     end
   end
   
@@ -38,12 +49,23 @@ class HumanController < ApplicationController
     @human.nid = params[:human][:nid]
     @human.group_id = params[:human][:group_id]
     @human.group_ids = params[:human][:group_ids]
-    begin
-        @human.save
-    rescue
-       flash[:notice] = "Successfully updated ..."
-       redirect_to(:action => 'list')
-    ensure 
+    flag=true
+    for i in @human.group
+      if i.human.count>3
+        flag=false
+      end
+    end
+    if flag
+        begin
+          @human.save
+      rescue
+         flash[:notice] = "Successfully updated ..."
+         redirect_to(:action => 'list')
+      ensure 
+      end
+    else   
+      flash[:notice] = "Error one of the group already filled ..."
+      redirect_to(:action => 'list')
     end
   end
   
