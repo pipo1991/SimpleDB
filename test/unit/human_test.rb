91645a4e55b4a3d36879aab07675392bf92e25a6
Patrick Ragi Helmi <Patricholamaro@yahoo.com>
2013-08-07 15:22:57 +0200

    Update unit\human_test

--- test/unit/human_test.rb
+++ test/unit/human_test.rb
@@ -4,4 +4,30 @@ class HumanTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def test_name
+    g=Human.new
+    g.name=nil
+    g.nid=4590
+    assert !g.save
+  end
+  
+  def test_nid
+    g=Human.new
+    g.name=nil
+    g.nid=4590
+    assert !g.save
+  end
+  
+  def group_not_more_than_4
+    g=Group.new
+    g.name="Temp"
+    for i in 0..10
+      h=Human.new
+      h.name="Human#{i}"
+      h.nid=178765873
+      h.group_id=1
+      g.human << h
+    end
+    assert !g.save
+  end
 end
