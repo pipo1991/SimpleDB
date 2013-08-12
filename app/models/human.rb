ee03129598c9470edf34e110aae5e145357ea26c
Patrick Ragi Helmi <Patricholamaro@yahoo.com>
2013-08-12 19:13:02 +0200

    Update the human model
    Update the human model by adding validation for checking that the number
    of group members don't exceed 4

--- app/models/human.rb
+++ app/models/human.rb
@@ -2,9 +2,20 @@ class Human < ActiveRecord::Base
   # attr_accessible :title, :body
   validates_presence_of :name 
   validates_presence_of :nid
-  belongs_to :group
+  validate :test_num
+  belongs_to :main_group, :class_name=>Group.name
   has_and_belongs_to_many :group
   attr_accessible :id,:nid, :name, :group_id ,:group , :group_ids
   scope :search ,lambda { |p| where (["name LIKE ?","%#{p}%"])}
+  
+  
+  def test_num
+    for i in self.group.each
+      if i.human.count > 4
+        errors.add(:base,"This is group have more than 4 members.")
+        break
+      end
+    end
+  end
   
 end
