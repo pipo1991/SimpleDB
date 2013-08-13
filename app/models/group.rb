d811782b94b42812ec6bb7a94673c67a948954e6
Patrick Ragi Helmi <Patricholamaro@yahoo.com>
2013-08-13 13:19:00 +0200

    Making the validation in the Group Model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,11 @@ class Group < ActiveRecord::Base
   validates_presence_of :name
   has_and_belongs_to_many :human
   attr_accessible :id,:name ,:human ,:human_ids
+  validate :group_size
+  
+  def group_size
+    if self.human.count >4
+      errors.add(:base,"This group have more than 4 members.")
+    end
+  end
 end
