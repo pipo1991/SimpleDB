3355a9051c9088a5dd928008be2ce93a3b548cae
Patrick Ragi Helmi <Patricholamaro@yahoo.com>
2013-08-13 12:56:34 +0200

    Deleting comment

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,5 @@
 class Group < ActiveRecord::Base
-  # attr_accessible :title, :body
   validates_presence_of :name
   has_and_belongs_to_many :human
   attr_accessible :id,:name ,:human ,:human_ids
-  
 end
--- test/unit/group_test.rb
+++ test/unit/group_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   def test_name
     g=Group.new
     g.name=nil
